679087e36ccc949cf959fcf7652c5c4aae9bd0cb
Fedir Kopakov <fedir.kopakov@gmail.com>
2025-08-25 18:16:46 +0300

    commit

--- lib/utils/mockCookies.ts
+++
@@ -1,7 +0,0 @@
-// Mock cookies object for server-side API routes
-export const mockCookies = {
-  get: () => undefined,
-  getAll: () => [],
-  set: () => {},
-  remove: () => {},
-};
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typedRoutes: true,
-};
-
-export default nextConfig;
-
-
